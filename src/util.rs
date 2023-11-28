e36d4f3c0986226551d887651e8c309cd660f3ee
Nuutti Kotivuori <naked@iki.fi>
2023-11-28 23:22:01 +0000

    Add errstr for easy error printing

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,9 @@
 use chumsky::prelude::*;
 
+pub fn errstr(errs: Vec<Simple<char>>) -> anyhow::Error {
+    anyhow::anyhow!(errs.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join("\n"))
+}
+
 pub fn integer() -> impl Parser<char, i64, Error = Simple<char>> {
     text::int(10).try_map(|n: String, span| n.parse::<i64>().map_err(|e| Simple::custom(span, format!("{}", e))))
 }
