311bc65d1d14b43caa5e3994d5daf6e1c3e19968
Nuutti Kotivuori <naked@iki.fi>
2023-12-26 08:57:14 +0000

    Optimize day 23

--- src/day23.rs
+++ src/day23.rs
@@ -1,8 +1,10 @@
 
 use anyhow::Result;
-use std::collections::{VecDeque, HashSet};
+use smallbitset::Set64;
+use std::collections::{VecDeque, HashMap};
 use pathfinding::matrix::{directions, Matrix};
-use petgraph::graphmap::DiGraphMap;
+use petgraph::graph::{DiGraph, NodeIndex};
+use petgraph::visit::EdgeRef;
 use test_case::test_case;
 
 fn parse_input(filename: &str) -> Result<Matrix<char>> {
@@ -64,56 +66,65 @@ fn neighbours2(map: &Matrix<char>, pos: (usize, usize)) -> impl IntoIterator<Ite
         .map(move |p| (p, -1))
 }
 
-fn simplify(input: &Matrix<char>) -> DiGraphMap<(usize, usize), i64> {
+fn simplify(input: &Matrix<char>) -> (NodeIndex, NodeIndex, DiGraph<(), i64>) {
     let start = (0, 1);
     let goal = (input.rows - 1, input.columns - 2);
+    let mut graph = DiGraph::new();
+    let mut nodemap = HashMap::new();
+    let startnode = graph.add_node(());
+    let goalnode = graph.add_node(());
+    nodemap.insert(start, startnode);
+    nodemap.insert(goal, goalnode);
     let mut branches = VecDeque::new();
-    branches.push_back((start, start, start, 0));
-    let mut graph = DiGraphMap::new();
+    branches.push_back((startnode, start, start, 0));
     while let Some((prev_branch, prev, next, cost)) = branches.pop_front() {
         if next == goal {
-            graph.add_edge(prev_branch, next, cost);
+            graph.add_edge(prev_branch, goalnode, cost);
             continue;
         }
         let posses: Vec<(usize, usize)> = neighbours2(&input, next).into_iter().filter(|&(p, _)| p != prev).map(|(p, _)| p).collect();
         if posses.len() == 1 {
             branches.push_back((prev_branch, next, posses[0], cost + 1));
         } else {
-            if let Some(_) = graph.add_edge(prev_branch, next, cost) {
-                continue;
-            }
-            for p in posses {
-                branches.push_back((next, next, p, 1));
+            if let Some(node) = nodemap.get(&next) {
+                if !graph.contains_edge(prev_branch, *node) {
+                    graph.add_edge(prev_branch, *node, cost);
+                    for p in posses {
+                        branches.push_back((*node, next, p, 1));
+                    }
+                }
+            } else {
+                let node = graph.add_node(());
+                nodemap.insert(next, node);
+                graph.add_edge(prev_branch, node, cost);
+                for p in posses {
+                    branches.push_back((node, next, p, 1));
+                }
             }
         }
     }
-    graph
+    (startnode, goalnode, graph)
 }
 
 #[test_case("inputs/example-23-1.txt" => matches Ok(154))]
 #[test_case("inputs/input-23.txt" => matches Ok(6230))]
 pub fn puzzle2(filename: &str) -> Result<i64> {
     let input = parse_input(filename)?;
-    let graph = simplify(&input);
+    let (startnode, goalnode, graph) = simplify(&input);
     let mut branches = VecDeque::new();
-    let start = (0, 1);
-    let goal = (input.rows - 1, input.columns - 2);
-    let mut seen = HashSet::new();
-    seen.insert(start);
-    branches.push_back((start, 0, seen));
+    let seen = Set64::singleton(startnode.index());
+    branches.push_back((startnode, 0, seen));
     let mut max_cost = 0i64;
     while let Some((node, cost, seen)) = branches.pop_front() {
-        if node == goal {
+        if node == goalnode {
             max_cost = max_cost.max(cost);
             continue;
         }
-        for (_, next, next_cost) in graph.edges(node) {
-            if seen.contains(&next) {
+        for er in graph.edges(node) {
+            if seen.contains(er.target().index()) {
                 continue;
             }
-            let mut seen = seen.clone();
-            seen.insert(next);
-            branches.push_back((next, cost + next_cost, seen));
+            branches.push_back((er.target(), cost + er.weight(), seen.add(er.target().index())));
         }
     }
     let ret = max_cost;
