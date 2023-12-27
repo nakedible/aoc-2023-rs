726b63ec2d598b93a564c7dc5020439c4d96971a
Nuutti Kotivuori <naked@iki.fi>
2023-12-27 02:43:30 +0000

    Day 2 parsing tweaks

--- src/day02.rs
+++ src/day02.rs
@@ -8,28 +8,27 @@ struct Game {
     green: i64,
 }
 
+fn parse_game(gamestr: &str) -> Game {
+    gamestr.split(", ").fold(Game::default(), |game, colorcount| {
+        let (count, color) = colorcount.split_once(' ').unwrap();
+        let count = count.parse().unwrap();
+        match color {
+            "red" => Game { red: count, ..game },
+            "blue" => Game { blue: count, ..game },
+            "green" => Game { green: count, ..game },
+            _ => panic!("Unknown color {}", color),
+        }
+    })
+}
+
 fn parse_input(filename: &str) -> Result<Vec<(i64, Vec<Game>)>> {
     let input = std::fs::read_to_string(filename)?;
-    let mut ret = Vec::new();
-    for line in input.lines() {
+    let ret = input.lines().map(|line| {
         let (gamenum, gamesstr) = line.split_once(": ").unwrap();
         let gameid = gamenum.strip_prefix("Game ").unwrap().parse().unwrap();
-        let mut games = Vec::new();
-        for gamestr in gamesstr.split("; ") {
-            let mut game = Game::default();
-            for colorcount in gamestr.split(", ") {
-                let (count, color) = colorcount.split_once(' ').unwrap();
-                match color {
-                    "red" => game.red = count.parse().unwrap(),
-                    "blue" => game.blue = count.parse().unwrap(),
-                    "green" => game.green = count.parse().unwrap(),
-                    _ => panic!("Unknown color {}", color),
-                }
-            }
-            games.push(game);
-        }
-        ret.push((gameid, games));
-    }
+        let games = gamesstr.split("; ").map(parse_game).collect();
+        (gameid, games)
+    }).collect();
     Ok(ret)
 }
 
@@ -60,7 +59,7 @@ pub fn puzzle2(filename: &str) -> Result<i64> {
     let input = parse_input(filename)?;
     let ret = input
         .iter()
-        .map(|(gameid, games)| {
+        .map(|(_, games)| {
             let (r, g, b) = games.iter().fold((0, 0, 0), |(red, green, blue), game| {
                 (
                     std::cmp::max(red, game.red),
@@ -68,9 +67,8 @@ pub fn puzzle2(filename: &str) -> Result<i64> {
                     std::cmp::max(blue, game.blue),
                 )
             });
-            (gameid, r * g * b)
+            r * g * b
         })
-        .map(|(_, power)| power)
         .sum::<i64>();
     Ok(ret)
 }
